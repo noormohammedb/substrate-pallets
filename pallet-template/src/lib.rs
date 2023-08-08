28a38ce6df321a3cd7be67247dd5494f3f2db4f6
Noor Mohammed B <noormohammedb@protonmail.com>
2023-08-08 21:39:52 +0400

    fix pallet-template
    std bug with typo in Cargo.toml
    enhanced pallet template

--- pallet-template/src/lib.rs
+++ pallet-template/src/lib.rs
@@ -3,23 +3,23 @@
 pub use pallet::*;
 
 #[frame_support::pallet(dev_mode)]
-
 pub mod pallet {
     use frame_support::pallet_prelude::*;
     use frame_system::pallet_prelude::*;
-    //
 
     #[pallet::pallet]
-    pub struct Pallet<T>(_);
+    pub struct Pallet<T>(PhantomData<T>);
 
     #[pallet::config]
     pub trait Config: frame_system::Config {
-        //
+        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
     }
 
-    // #[pallet::event]
-    // pub enum Event<T: Config> {
-    // }
+    #[pallet::event]
+    #[pallet::generate_deposit(pub(super)fn deposit_event)]
+    pub enum Event<T: Config> {
+        //
+    }
 
     #[pallet::error]
     pub enum Error<T> {
@@ -28,7 +28,11 @@ pub mod pallet {
 
     #[pallet::call]
     impl<T: Config> Pallet<T> {
-        //
+        #[pallet::call_index(1)]
+        #[pallet::weight(0)]
+        pub fn do_something(origin: OriginFor<T>) -> DispatchResult {
+            Ok(())
+        }
     }
 
     impl<T: Config> Pallet<T> {
