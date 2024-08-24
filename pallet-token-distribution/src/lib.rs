02297cf32d1824c44208eb27ad21822ea6be9667
Noor Mohammed <noormohammedb@protonmail.com>
2024-08-24 13:22:34 +0400

    basic setup for new pallet token distribution

---
+++ pallet-token-distribution/src/lib.rs
@@ -0,0 +1,22 @@
+//! A shell pallet built with [`frame`].
+//!
+//! To get started with this pallet, try implementing the guide in
+//! <https://paritytech.github.io/polkadot-sdk/master/polkadot_sdk_docs/guides/your_first_pallet/index.html>
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use polkadot_sdk_frame::prelude::*;
+
+// Re-export all pallet parts, this is needed to properly import the pallet into the runtime.
+pub use pallet::*;
+
+#[polkadot_sdk_frame::pallet]
+pub mod pallet {
+	use super::*;
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config {}
+
+	#[pallet::pallet]
+	pub struct Pallet<T>(_);
+}
